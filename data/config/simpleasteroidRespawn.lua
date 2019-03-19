f8fed3acdb130756822214f4b765a5ccbb28c726
Laserzwei <lasernr2@gmail.com>
2019-03-19 11:28:50 +0100

    new workshop standard

---
+++ data/config/simpleasteroidRespawn.lua
@@ -0,0 +1,7 @@
+local Config = {}
+-- Time between respawn tries in seconds
+Config.respawnTime = 1800   -- Default: 1800 (= 30 min)
+-- Percentage of the original amount, of rich asteroids, to respawn. (0.01 = 1%)
+Config.respawnAmount = 0.10     --Defaul: 0.10
+
+return Config
